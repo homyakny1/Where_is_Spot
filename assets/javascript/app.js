844d0121f1490b94653417c4986fda06acf08f9b
Eugene Khom <homyakny@gmail.com>
2017-12-14 05:20:56 -0500

    added login, need to test

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,57 @@
-var queryURL = "https://zoning.io/en/api/1.0/zones.json?code=C-B";
+//Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBHk95-lubVI3oVjkYiWihfYzcL3e5eX8g",
+    authDomain: "bc-project1.firebaseapp.com",
+    databaseURL: "https://bc-project1.firebaseio.com",
+    projectId: "bc-project1",
+    storageBucket: "bc-project1.appspot.com",
+    messagingSenderId: "947495581665"
+  };
 
-$.ajax({
-    url: queryURL,
-    method: 'GET',
-    jurisdictions: {
-        id: 1292,
-        slug: "beach-commercial",
-        name: "Beach Commercial",
-        display_name: "C-B - Beach Commercial",
-        description: "The purpose of this district is to provide for commercial uses which are primarily coastal-dependent in nature and which serve tourists and visitors to the Santa Cruz coastal recreational areas. Also, to provide commodities and services to residents of such areas. The C-B District shall be applied only in areas designated in the General Plan and the Local Coastal Program.",
-        municipal_code_url: "http://www.codepublishing.com/ca/santacruz/?santacruz24/SantaCruz2410.html#24.10.1100",
-        code: "C-B",
-        overlay: false
-    },
-}).done(function (response) {
+  firebase.initializeApp(config);
 
-    console.log(response);
+  var uiConfig = {
+    signInSuccessUrl: 'home.html',
+    signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    ],
+  };
 
+  // Initialize the FirebaseUI Widget using Firebase.
+  var ui = new firebaseui.auth.AuthUI(firebase.auth());
+  // The start method will wait until the DOM is loaded.
+  ui.start('#firebaseui-auth-container', uiConfig);
+
+// //   Add login event
+// $("#btnLogin").on("click", function(){
+//     // Get email and password
+//     var email = $("#txtEmail").val().trim();
+//     var pass = $("#txtPassword").val().trim();
+//     var auth = firebase.auth();
+//     //Sign In
+//     var promise = auth.signInWithEmailAndPassword(email, pass);
+//     console.log("pressed")
+//     promise.catch(e => console.log(e.message));
+// })
+
+// $("#btnSignUp").on("click", function(){
+//     // Get email and password
+//     var email = $("#txtEmail").val().trim();
+//     var pass = $("#txtPassword").val().trim();
+//     var auth = firebase.auth();
+//     //Sign In
+//     var promise = auth.createUserWithEmailAndPassword(email, pass);
+//     console.log("pressed")
+//     promise.catch(e => console.log(e.message));
+//     auth.createUserWithEmailAndPassword(email, pass);
+// })
+// Add a realtime listener
+firebase.auth().onAuthStateChanged(firebaseUser => {
+    if(firebaseUser){
+        location.href = "home.html";
+        console.log("Welcome"+user.displayName)
+    } else{
+        console.log("Not Logged in")
+    }
 });
