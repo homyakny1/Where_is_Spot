40b9a748a8f3613c9817e5f63687583ab7930efd
Eugene Khom <homyakny@gmail.com>
2017-12-14 20:34:37 -0500

    test

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,31 +6,16 @@ var config = {
     projectId: "bc-project1",
     storageBucket: "bc-project1.appspot.com",
     messagingSenderId: "947495581665"
-  };
+};
 
-  firebase.initializeApp(config);
-
-  var uiConfig = {
-    signInSuccessUrl: 'home.html',
-    signInOptions: [
-      // Leave the lines as is for the providers you want to offer your users.
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    ],
-  };
-
-  // Initialize the FirebaseUI Widget using Firebase.
-  var ui = new firebaseui.auth.AuthUI(firebase.auth());
-  // The start method will wait until the DOM is loaded.
-  ui.start('#firebaseui-auth-container', uiConfig);
-
-  $("#btnLogout").on("click", function(){
-      firebase.auth().signOut();
-  })
+$("#btnLogout").on("click", function () {
+    firebase.auth().signOut();
+})
 
 firebase.auth().onAuthStateChanged(firebaseUser => {
-    if(firebaseUser){
-        console.log("Welcome"+user.displayName)
-    } else{
+    if (firebaseUser) {
+        console.log("Welcome" + user.displayName)
+    } else {
         console.log("Not Logged in")
     }
 });
